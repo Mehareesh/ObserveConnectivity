10b1a6b76d8065c8021eee5779e75f542c9d3d8b
Mehareesh Meruga <mehareeshm@gmail.com>
2022-08-11 15:42:29 +0530

    Removed unused imports

--- app/src/main/java/com/soumeru/observeconnectivity/MainActivity.kt
+++ app/src/main/java/com/soumeru/observeconnectivity/MainActivity.kt
@@ -5,15 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.soumeru.observeconnectivity.ui.theme.ObserveConnectivityTheme
 
 class MainActivity : ComponentActivity() {
